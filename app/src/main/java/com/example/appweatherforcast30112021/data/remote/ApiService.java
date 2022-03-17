3c2ea70b403684929647237d97c1edcaffa67186
Your Name <you@example.com>
2022-03-17 18:17:31 +0700

    update

--- app/src/main/java/com/example/appweatherforcast30112021/data/remote/ApiService.java
+++ app/src/main/java/com/example/appweatherforcast30112021/data/remote/ApiService.java
@@ -1,10 +1,26 @@
 package com.example.appweatherforcast30112021.data.remote;
 
+import com.example.appweatherforcast30112021.data.model.WeatherResponse;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface ApiService {
 
+    // Truyền theo Query : (@Query)
+    // http://api.openweathermap.org/data/2.5/weather?appid=86183a23377ed034aef7aad102f43d64&units=metric&q=Hanoi
+
+    // Truyền theo Params : (@Params)
+    // http://api.openweathermap.org/data/2.5/weather/temp/{time}/data
+    // @Path("time") String time
+
     @GET("data/2.5/weather")
-    Call<>
+    Call<WeatherResponse> getTempCity(
+            @Query("appid") String appid,
+            @Query("units") String units,
+            @Query("q") String q
+    );
+
+
 }
